48703f750462ded6c7423924df39f8e6eed80792
Ltoroc <ltoroc97@gmail.com>
2022-12-11 17:25:26 -0500

    Add Exercise One

---
+++ Practica_N1/Ejercicio_Variables.js
@@ -0,0 +1,12 @@
+
+/*
+
+Ejercicios Variables y Tipos de Variables:
+
+    Crea las variables como se indican a continuación:
+    Un número llamado myNumber que contenga su edad, como variable.
+    Un string llamado firstName que contiene su(s) nombre(s), como constante.
+    Un string llamado lastName que contiene sus apellidos, como constante.
+    Un booleano llamado myBoolean que contiene el valor false, libre.
+*/
+

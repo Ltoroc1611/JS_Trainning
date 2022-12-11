a359a39713550b328f7b618cdcc0636b5837ccdd
Ltoroc <ltoroc97@gmail.com>
2022-12-11 18:16:31 -0500

    Exercise Two finished

---
+++ Practica_N1/Ejercicio_Condicional.js
@@ -0,0 +1,32 @@
+/*
+
+Ejercicios de Condicionales:
+
+Pega este código en un archivo JavaScript y dinos cuál es el valor final de la variable subsidioAdicional:
+
+
+let compraRealizada = false;
+let subsidioAdicional = 0;
+
+if (compraRealizada === true) 
+{
+  subsidioAdicional = 10;
+} else {
+  subsidioAdicional = 5;
+
+
+Debes construir un if que compruebe si el número myNumber es igual a 42. Si es así, la función debe imprimir mediante console.log 
+la cadena ‘Es mi edad’. Si myNumber no es igual a 42, imprimir mediante console.log la cadena ‘No es mi edad’.
+
+Tenemos una variable con la edad de una persona, y queremos conocer si esta es mayor o menor de edad. 
+Use un operador ternario donde, si es mayor de edad el mensaje sea ‘adulto’ y si no lo es ‘menor’. Finalmente muéstrelo en consola.
+
+*/
+
+let myNumber = 42;
+
+if( myNumber == true){
+    console.log ("Es mi edad");
+}else{
+    console.log("No es mi edad");
+}
---
+++ Practica_N1/Ejercicio_Operadores.js
@@ -0,0 +1,22 @@
+/*
+
+Indica al final con un comentario // El doble slash sirve para añadir un comentario en JavaScript , que valor devuelve las siguiente operaciones
+
+AND : &&
+OR : ||
+NOT !
+
+*/
+
+
+var a1 =  true && true;     // t && t devuelve - True
+var a2 =  true && false;    // t && f devuelve - False
+var a3 = false && true;     // f && t devuelve - True
+
+var o1 =  true || true;     // t || t devuelve  - True    
+var o2 = false || true;     // f || t devuelve - False
+var o3 =  true || false;    // t || f devuelve - Trux|e
+
+var n1 = !true;  // !t devuelve - False
+var n2 = !false; // !f devuelve - True
+var n3 = !'Cat'; // !t devuelve - Perro / False
